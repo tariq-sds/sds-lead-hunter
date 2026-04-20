f4215534c73ed3033d4215091001201280670831
tariq-sds <tariq@sonicdesignstudios.com>
2026-04-20 15:50:08 +0100

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SDSLeadHunter from './SDSLeadHunter'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SDSLeadHunter />
+  </React.StrictMode>
+)
